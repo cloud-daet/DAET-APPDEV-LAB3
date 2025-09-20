71d1c8f4c079c82fe48fb4b5ae22268ccd1c127e
cloud-daet <cloudsedgwickbalute.daet24@bicol-u.edu.ph>
2025-09-20 16:19:13 +0800

    Added Star Pattern Func

--- DAET-LabAct3.js
+++ DAET-LabAct3.js
@@ -20,3 +20,15 @@ function calculateGrade(score) {
         return "F"
     }
 }
+
+// Star Pattern
+function showStars(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let pattern = " ";
+        for (let j = 1; j <= i; j++) {
+            pattern += "*";
+        }
+        console.log(pattern);
+    }
+}
+
